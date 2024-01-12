bfaff3082e3a5224d0ab227d869f13af1bf6872b
TheoC26 <91738250+TheoC26@users.noreply.github.com>
2024-01-11 22:47:14 -0500

    next

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 const matterContainer = document.getElementById("matter-container");
 const memberNumber = document.getElementById("memberNumber");
 
+const colors = ["#ef476f", "#ffd166", "#06d6a0", "#118ab2", "#073b4c"];
+
+const size = 200;
+const amount = 5;
+
 let engine = new Matter.Engine.create();
 let render = Matter.Render.create({
   element: matterContainer,
@@ -36,95 +41,97 @@ let rightWall = Matter.Bodies.rectangle(
 );
 
 let objects = [];
-for (let i = 0; i < 20; i++) {
+for (let i = 0; i < amount; i++) {
   objects.push(
     Matter.Bodies.rectangle(
       Math.random() * matterContainer.clientWidth,
       Math.random() * matterContainer.clientHeight,
-      Math.random() * 100,
-      Math.random() * 100,
+      Math.random() * size,
+      Math.random() * size,
       {
         restitution: 0.8,
-        // render: {
-        //   fillStyle: "red",
-        // },
+        render: {
+          fillStyle: colors[Math.floor(Math.random() * colors.length)],
+        },
       }
     )
   );
 }
 
 // do the same with circles
-for (let i = 0; i < 20; i++) {
+for (let i = 0; i < amount; i++) {
   objects.push(
     Matter.Bodies.circle(
       Math.random() * matterContainer.clientWidth,
       Math.random() * matterContainer.clientHeight,
-      Math.random() * 100,
+      Math.random() * size,
       {
         restitution: 0.8,
-        // render: {
-        //   fillStyle: "red",
-        // },
+        render: {
+          fillStyle: colors[Math.floor(Math.random() * colors.length)],
+        },
       }
     )
   );
 }
 // do the same with triangles
-for (let i = 0; i < 20; i++) {
+for (let i = 0; i < amount; i++) {
   objects.push(
     Matter.Bodies.polygon(
       Math.random() * matterContainer.clientWidth,
       Math.random() * matterContainer.clientHeight,
       3,
-      Math.random() * 100,
+      Math.random() * size,
       {
         restitution: 0.8,
-        // render: {
-        //   fillStyle: "red",
-        // },
+        render: {
+          fillStyle: colors[Math.floor(Math.random() * colors.length)],
+        },
       }
     )
   );
 }
 
-// objects.push(Matter.Bodies.rectangle(
-//   Math.random() * matterContainer.clientWidth,
-//   40,
-//   800,
-//   60,
-//   {
-//     restitution: 0.8,
-//     render: {
-//       fillStyle: "transparent",
-//       text: {
-//         content: "Creative Coding Fest",
-//         color: "black",
-//         size: 72,
-//         family: "Arial",
-//       },
-//     },
-//   }
-// ))
-// objects.push(
-//   Matter.Bodies.rectangle(
-//     Math.random() * matterContainer.clientWidth,
-//     40,
-//     260,
-//     60,
-//     {
-//       restitution: 0.8,
-//       render: {
-//         fillStyle: "transparent",
-//         text: {
-//           content: "January 18th 2024",
-//           color: "black",
-//           size: 32,
-//           family: "Arial",
-//         },
-//       },
-//     }
-//   )
-// );
+objects.push(
+  Matter.Bodies.rectangle(
+    Math.random() * matterContainer.clientWidth,
+    500,
+    700,
+    60,
+    {
+      restitution: 0.8,
+      render: {
+        fillStyle: "transparent",
+        text: {
+          content: "Creative Coding Fest",
+          color: "black",
+          size: 72,
+          family: "Arial",
+        },
+      },
+    }
+  )
+);
+objects.push(
+  Matter.Bodies.rectangle(
+    Math.random() * matterContainer.clientWidth,
+    400,
+    260,
+    60,
+    {
+      restitution: 0.8,
+      render: {
+        fillStyle: "transparent",
+        text: {
+          content: "January 18th 2024",
+          color: "black",
+          size: 32,
+          family: "Arial",
+        },
+      },
+    }
+  )
+);
 
 let mouse = Matter.Mouse.create(render.canvas);
 let mouseConstraint = Matter.MouseConstraint.create(engine, {
